35a7040cf260118e94748a16684759f678c03c64
phunghoanglong <s3965673@rmit.edu.vn>
2024-08-06 02:05:59 +0700

    Initial Commit

---
+++ Valorant Agent List/Valorant_Agent_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Valorant_Agent_ListApp.swift
+//  Valorant Agent List
+//
+//  Created by Long Phung on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Valorant_Agent_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
